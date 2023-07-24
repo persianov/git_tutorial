b9c79f19c93b915c76d704c1dceb248709024107
Mikhail Persianov <persianov.m@gmail.com>
2023-07-24 23:16:18 +0300

    new hello

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args){
-        stringPrepare("hello2");
+        stringPrepare("hello5555");
     }
 
     public static void stringPrepare(String toPrint){
