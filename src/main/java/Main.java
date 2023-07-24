ca460d5ba171580291d62c56a6b975c66cfdc25f
Mikhail Persianov <persianov.m@gmail.com>
2023-07-24 23:05:05 +0300

    modified

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello");
+        stringPrepare("hello2");
+    }
+
+    public static void stringPrepare(String toPrint){
+        System.out.println(toPrint);
     }
 }
